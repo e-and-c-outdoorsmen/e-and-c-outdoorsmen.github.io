978610db6cde237b5e87d06d68614c84d8b2f30d
e-and-c-outdoorsmen <79102217+e-and-c-outdoorsmen@users.noreply.github.com>
2021-02-15 10:19:06 -0600

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,25 @@
+$(document).ready(function(){
+    $grid = $('.list').isotope({
+      // options
+      itemSelector: '.list__item',
+      layoutMode: 'masonry',
+      masonry: {
+        isFitWidth: true 
+      }
+    });
+    // filter items on button click
+    $('.js-filter').on( 'click', 'button', function() {
+      var filterValue = $(this).attr('data-filter');
+      $grid.isotope({ filter: filterValue });
+      $( '.js-filter button' ).removeClass( 'is-active' );
+      $( this ).addClass( 'is-active' );
+    });
+
+    //navigation
+    $('nav a[href*="#"]').on('click', function (){
+        $('html, body').animate({
+            scrollTop: $($(this).attr('href')).offset().top-100},
+            2000);
+        });
+        
+  });
